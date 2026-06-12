8295ba99ec080626f74035edee784937a89f596d
tutranbel <mac@Mac.lan>
2026-06-13 03:52:57 +0700

    chore: remove legacy 4h models and obsolete scratch scripts

--- search_yf.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yf
-for tk in ['^TNX', '^FVX', '^IRX']:
-    ticker = yf.Ticker(tk)
-    try:
-        print(tk, ticker.history(period="1d")['Close'].iloc[-1])
-    except:
-        pass
